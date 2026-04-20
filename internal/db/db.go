9421bbf1246006db07df00777a1125be5dbc088f
Marco Carbajal <car23025@uvg.edu.gt>
2026-04-20 00:20:39 -0600

    Se estableció la conexión con sqlite y el esquema de la tabla series

---
+++ internal/db/db.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	_ "embed"
+
+	_ "modernc.org/sqlite"
+)
+
+var schema string
+
+// Abre la conexión a SQLite y verifica que responde
+func Open(path string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", path)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// Ejecuta el schema embebido
+func Migrate(db *sql.DB) error {
+	_, err := db.Exec(schema)
+	return err
+}
