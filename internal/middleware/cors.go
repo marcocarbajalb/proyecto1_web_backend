5f08e47bc8af2ed06e7440ea3e44d856b13a550a
Marco Carbajal <car23025@uvg.edu.gt>
2026-04-23 08:47:39 -0600

    Permitir configurar origen cors por variable de entorno

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -1,10 +1,18 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CORS(next http.Handler) http.Handler {
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+	if allowedOrigin == "" {
+		allowedOrigin = "*"
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
